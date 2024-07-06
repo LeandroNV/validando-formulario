858e14586beac704e4c68c1bb5c2d2b53fbbbcec
leonicvarc <leonicvarc@gmail.com>
2024-07-06 19:36:07 +0000

    val-date

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import esUnCUIT from "./validar-cuit.js";
+import esMayorDeEdad from "./validar-edad.js";
 
 const camposDeFormulario = document.querySelectorAll("[required");
-
 
 camposDeFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificarCampo(campo));
@@ -10,5 +10,8 @@ camposDeFormulario.forEach((campo) => {
 function verificarCampo(campo) {
   if (campo.name == "cuil" && campo.value.length >= 11) {
     esUnCUIT(campo);
+  }
+  if (campo.name == "fecha_nacimiento" && campo.value != "") {
+    esMayorDeEdad(campo);
   }
 }
--- js/validar-cuit.js
+++ js/validar-cuit.js
@@ -1,10 +1,24 @@
 export default function esUnCUIT(campo) {
-  /* Reemplazamos la expresion regular por un espacio vacio */
   const cuit = campo.value.replace(/\-/g, "");
-  console.log(cuit);
-  tieneCaracteresRepetidos(cuit);
-  console.log(tieneCaracteresRepetidos(cuit));
+  console.log(
+    tieneCaracteresRepetidos(cuit),
+
+    validarPrimerosDigitosCUIT(cuit),
+
+    validarDigitoVerificador(cuit)
+  );
+
+  if (!tieneCaracteresRepetidos(cuit)) {
+    if (validarPrimerosDigitosCUIT(cuit) && validarDigitoVerificador(cuit)) {
+      console.log("el Cuit existe");
+    } else {
+      console.log("el Cuit NO existe");
+    }
+  } else {
+    console.log("Números repetidos");
+  }
 }
+
 
 function tieneCaracteresRepetidos(cuit) {
   const numerosRepetidos = [
@@ -20,4 +34,35 @@ function tieneCaracteresRepetidos(cuit) {
     "99999999999",
   ];
   return numerosRepetidos.includes(cuit);
+}
+
+/* Validar primeros digitos */
+function validarPrimerosDigitosCUIT(cuit) {
+  let primerosDigitos = Number(cuit.slice(0, 2)); //o substr
+  let digitosValidos = [20, 23, 24, 27, 30, 33, 34];
+
+  return digitosValidos.includes(primerosDigitos); // Verificar los primeros dos dígitos
+}
+
+function validarDigitoVerificador(cuit) {
+  let digitoVerificador = Number(cuit.slice(-1));
+
+  // console.log(digitoVerificador);
+  let digitos = cuit.substr(0, 10).split("").map(Number);
+
+  //console.log(digitos);
+
+  let acumulado = 0;
+  let factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+
+  for (let i = 0; i < digitos.length; i++) {
+    acumulado += digitos[i] * factores[i];
+  }
+
+  let verificador = 11 - (acumulado % 11);
+  if (verificador === 11) {
+    verificador = 0;
+  }
+
+  return digitoVerificador === verificador;
 }
